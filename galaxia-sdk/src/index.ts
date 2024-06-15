5748ff519c80fb7eaf0e8e1ca41fed1f4df54420
KOSASIH <kosasihg88@gmail.com>
2024-06-15 11:45:29 +0700

    Update index.ts

--- galaxia-sdk/src/index.ts
+++ galaxia-sdk/src/index.ts
@@ -1,3 +1,6 @@
-import { GalaxiaSDK } from './sdk';
+import { GalaxiaClient } from './GalaxiaClient';
+import { GalaxiaWallet } from './GalaxiaWallet';
+import { GalaxiaToken } from './GalaxiaToken';
+import { GalaxiaFarm } from './GalaxiaFarm';
 
-export { GalaxiaSDK };
+export { GalaxiaClient, GalaxiaWallet, GalaxiaToken, GalaxiaFarm };
