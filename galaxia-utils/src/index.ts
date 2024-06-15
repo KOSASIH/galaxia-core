9b57c9a96fdb58040c31aff951963903e4a88d42
KOSASIH <kosasihg88@gmail.com>
2024-06-15 10:57:56 +0700

    Create index.ts

---
+++ galaxia-utils/src/index.ts
@@ -0,0 +1,5 @@
+import { Logger } from './logger';
+import { promisify } from './promisify';
+import { hash, encrypt, decrypt } from './utils';
+
+export { Logger, promisify, hash, encrypt, decrypt };
