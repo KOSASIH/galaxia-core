237aa60629eba69d81bb89eccd1c462658216ed8
KOSASIH <kosasihg88@gmail.com>
2024-06-15 12:52:33 +0700

    Create user.py

---
+++ models/user.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), unique=True)
+    email = Column(String(120), unique=True)
+
+    def __repr__(self):
+        return f"User(username={self.username}, email={self.email})"
